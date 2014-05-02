34320475e31f280ce9d3360856d85ba3686a542b
EdbertChan <edbert.chan@yahoo.com>
2014-05-01 20:40:21 -0700

    Simulation Code complete: Its really shitty

--- app/controllers/get_restaurant_lists_controller.rb
+++ app/controllers/get_restaurant_lists_controller.rb
@@ -1,5 +1,8 @@
 #include GetRestaurantListHelper
 require 'net/http'
+require 'rubygems'
+require 'json'
+require 'active_support'
 class GetRestaurantListsController < ApplicationController
 
 
@@ -32,6 +35,7 @@ class GetRestaurantListsController < ApplicationController
     #nonproduction code. Just used for demoing
 
 #sample SSH call to google maps api
+
 uri = URI('https://maps.googleapis.com/maps/api/directions/json?origin=LaJolla&destination=SanDiego&sensor=false&key=AIzaSyAAe8uFG4L8f_LYe-7etsNwdXraAUxIcPs')
 http = Net::HTTP.new(uri.host, uri.port)
 
@@ -39,11 +43,64 @@ http.use_ssl = true
 http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 request = Net::HTTP::Get.new(uri.request_uri)
 
-response = http.request(request)
+responseRoute = http.request(request)
+
+
     
+    
+=begin   
+    latPoints = [ "32.84020539999999",
+         "32.8404957",
+         "32.841919",
+         "32.8500026",
+         "32.841919",
+         "32.840688",
+         "32.8387322",
+         "32.840688",
+         "32.7268972",
+         "32.7245536"
+      ]
+    longPoints = [ "-117.2737146",
+         "-117.2724922",
+         "-117.272949",
+         "-117.2515528",
+         "-117.272949",
+         "-117.2378148",
+         "-117.2365587",
+        "-117.2378148",
+        "-117.1675917",
+        "-117.1657617"
+      ]
+=end
+    latPoints = [ "32.84020539999999",
+         "32.841919",
+      ]
+    longPoints = [ "-117.2737146",
+         "-117.272949",
+      ]
+
+array = Array.new
+for i in 0...latPoints.size
+ url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + latPoints[i] +
+  ","+longPoints[i] + "&radius=500&types=food&sensor=false&key=AIzaSyAAe8uFG4L8f_LYe-7etsNwdXraAUxIcPs"
+  
+  uri = URI(url)
+  http = Net::HTTP.new(uri.host, uri.port)
+
+http.use_ssl = true
+http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+request = Net::HTTP::Get.new(uri.request_uri)
+
+responsePlace = http.request(request)
+    array.push(responsePlace.body)
+end
+
+
+
+    jsonStr = {:routes =>  ActiveSupport::JSON.decode(responseRoute.body), :place1 =>  ActiveSupport::JSON.decode(array[0]), :place2 =>  ActiveSupport::JSON.decode(array[1])}
  #return stuff
- render json: response.body
-    
+
+ render json: jsonStr
         #render json: @get_restaurant_lists, status: 422
     
   end
