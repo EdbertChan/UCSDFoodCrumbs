9017fd22fafbbd46864bd1fded58957b206fadc7
Dheeraj Navani <dheerajnavani@gmail.com>
2014-05-25 13:52:16 -0700

    tested resizeBoxesToCircles

--- app/models/testing_algorithm.rb
+++ app/models/testing_algorithm.rb
@@ -6,15 +6,21 @@ return "this is the return"
 end
 
 def self.algorithm(maxRouteBoxer,userRouteBoxer,searchString)
+    queryList = resizeBoxesToCircles(maxRouteBoxer,searchString) # setup query
+    placesResults = placesQuery(queryList)
+    placesList = filterResults(userRouteBoxer,placesResults)
+    return placesList
+end
+
+def self.resizeBoxesToCircles(maxRouteBoxer,searchString)
     # radius in km
     maxRadius = 50
     
     queryList = Array.new
     # while there are boxes in maxRouteBoxer
     while( maxRouteBoxer.length > 0 )
-    {
         # pop a box
-        latlngBounds = maxRouteBoxer.pop()
+        latlngBounds = maxRouteBoxer.pop
     
         # calculate diagonal distance of box in km
         radiusOfEarth = 6371
@@ -28,67 +34,67 @@ def self.algorithm(maxRouteBoxer,userRouteBoxer,searchString)
             Math.sin(changeInLng/2) * Math.sin(changeInLng/2);
         
         c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-        d = R * c
+        d = radiusOfEarth * c
         
         radius = d/2;    
         midLat = latlngBounds[0][0] + (latlngBounds[1][0] - latlngBounds[0][0])/2
         midLng = latlngBounds[0][1] + (latlngBounds[1][1] - latlngBounds[0][1])/2
         
         if( radius <= maxRadius )
-        {
             queryList.push([midLat,midLng,radius,searchString])
-        }
         else
-        {
             if( changeInLat > changeInLng )
-            {
-                # left box
+                # top box
                 maxRouteBoxer.push([[ midLat,latlngBounds[0][1]],
                                     [ latlngBounds[1][0], latlngBounds[1][1]]])
-                # right box
+                # bottom box
                 maxRouteBoxer.push([[ latlngBounds[0][0], latlngBounds[0][1]],
                                     [ midLat, latlngBounds[1][1]]])
-            }
             else
-            {
-                # top box
+                # left box
                 maxRouteBoxer.push([[latlngBounds[0][0],latlngBounds[0][1]],
                                     [latlngBounds[1][0],midLng]])
-                # bottom box
+                # right box
                 maxRouteBoxer.push([[latlngBounds[0][0],midLng],
                                     [latlngBounds[1][0],latlngBounds[1][1]]])
-            }
-        }
-    } # end while
+            end
+        end
+    end #while
 
+    return queryList
+end
+
+def self.placesQuery(queryList)
     # query Places.getPlaces
     placesList = Array.new
-    for( i = 0; i < queryList.length; i++ ) {
+    for i in 0..queryList.length-1
         placesList.push(Places.getPlaces(queryList[i][0],queryList[i][1],queryList[i][2],queryList[i][3]))
-    }
+    end
 
-    placesList = placesList.flatten(1)
-    placesList = placesList.uniq
+    placesList = placesList.flatten(1) # combine requests
+    placesList = placesList.uniq # remove duplicates    
 
+    return placesList
+end
+
+def self.filterResults( userRouteBoxer, placesList )
     # filter output
-    for( i = 0; i < placesList.length; i++ ) {
+    for i in 0..(placesList.length-1)
         flag = 0
-        for( j = 0; j < userRouteBoxer; j++ ) {
+        for j in 0..(userRouteBoxer-1)
             if( placesList[i]["lat"] >= userRouteBoxer[j][0][0] &&
                 placesList[i]["lat"] <= userRouteBoxer[j][1][0] &&
                 placesList[i]["lng"] >= userRouteBoxer[j][0][1] &&
                 placesList[i]["lng"] <= userRouteBoxer[j][1][1] )
-            {
                 flag = 1
                 break
-            }
-        }
-        if( flag == 0 ) {
+            end
+        end
+        if flag == 0
             placesList.delete_at(i)
-            i--;
-        }
-    }
-
+            i = i-1
+        end
+    end
     return placesList
 end
 
