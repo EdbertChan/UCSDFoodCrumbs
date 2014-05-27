7c0a54eb96127ab816efd1e699dcc1302aef6e94
Dheeraj Navani <dheerajnavani@gmail.com>
2014-05-27 15:45:19 -0700

    jesus integration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140526040255) do
+ActiveRecord::Schema.define(version: 20140527224105) do
+
+  create_table "details", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "places", force: true do |t|
     t.datetime "created_at"
