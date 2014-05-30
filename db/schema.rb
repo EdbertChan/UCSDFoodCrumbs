38d75ed5ba09939f27b4f71e6280dd1e5266c57f
Jesus Horta <jhorta13@gmail.com>
2014-05-29 21:44:25 -0700

    tested merged branch

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140519200103) do
+ActiveRecord::Schema.define(version: 20140529223628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,11 @@ ActiveRecord::Schema.define(version: 20140519200103) do
   end
 
   create_table "places", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "places_finders", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
