3691ef8c1faae9fe6dd148edde3b666d078603a0
EdbertChan <edbert.chan@yahoo.com>
2014-05-30 22:26:44 -0400

    Fixed PHP error posibly

--- app/controllers/get_restaurant_lists_controller.rb
+++ app/controllers/get_restaurant_lists_controller.rb
@@ -54,6 +54,7 @@ class GetRestaurantListsController < ApplicationController
 
     #boxer has these as a json. We're going to convert these to an array of array of array of floats
     arrayOfBoxCoordinatesGoogleMax = RouteBoxerHelper.convert_route_boxes_json_to_array(jsonArrayofGoogleMaxRouteBoxes)
+
 =begin
     #3. Algoirthm
 arrayOfBoxCoordinates = RouteBoxerHelper.get_route_boxes("foo", 5)
