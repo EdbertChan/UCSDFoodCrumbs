e88a7bd3043ba30aaf1d491c712adff8c6d52af5
Jesus Horta <jhorta13@gmail.com>
2014-05-30 18:39:42 -0700

    old api key

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -1,6 +1,6 @@
 require 'json'
 module PlacesHelper
-    API_KEY = 'AIzaSyCS9C8uXjI5_qL5Z31gXj9Zsp5q1QuXpgM'
+    API_KEY = 'AIzaSyC0ht6wjGCRpt9DyIiUrU2FQWxUOfYOZt8'
 
     def self.build_url(lat, long, radius, type )
         if type == ""
